f7750d4b36c24d2804cbcbec13e88ea206d99c74
zachwhy <73175184+zachwhy@users.noreply.github.com>
2021-06-04 11:10:33 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -81,14 +81,14 @@ class Predict(Resource):
             response_container.append(i["correct"])
 
         acc = sum(response_container)/len(response_container)
-        acc2 = round(acc,3)
+        acc2 = round(acc,2)
 
         # acc_container.append(acc)
 
         if acc2 == 0.8:
             reward = 100
         else:
-            reward = -abs((0.8 - acc2)*100)
+            reward = -abs((0.8 - acc)*100)
 
         # reward_container.append(reward)
 
